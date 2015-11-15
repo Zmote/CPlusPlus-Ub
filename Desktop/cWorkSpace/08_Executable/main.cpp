5376f28dd38a14acee2ac34ccaf3f57ea8751ea8
Zafer <shodaime.hikarikage@gmail.com>
2015-11-15 12:10:40 +0100

    sunday algos

--- Desktop/cWorkSpace/08_Executable/main.cpp
+++ Desktop/cWorkSpace/08_Executable/main.cpp
@@ -1,3 +1,10 @@
-int main(){
+#include "palindrom.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
 
+int main(){
+//printPalindromes_fromFile(std::cout,"linuxwords.sql");
+printAnagrams(std::cout,getValidWords(generate_anagrams(std::cin)));
 }
