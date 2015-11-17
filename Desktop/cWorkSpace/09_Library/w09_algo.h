96118e7130cbc90e63ee04b105d7ef523d6e91f4
Zafer <shodaime.hikarikage@gmail.com>
2015-11-17 20:41:11 +0100

    ub9 first exercises finish, wordle started

---
+++ Desktop/cWorkSpace/09_Library/w09_algo.h
@@ -0,0 +1,11 @@
+#ifndef W09_ALGO_H_
+#define W09_ALGO_H_
+#include <iosfwd>
+
+int wcount(std::istream& in);
+int wdiffcount(std::istream& in);
+void wfavorite(std::ostream& out, std::istream& in);
+void wordle(std::ostream& out, std::istream& in);
+
+
+#endif /* W09_ALGO_H_ */
